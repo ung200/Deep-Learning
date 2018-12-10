6b7c3d68aebd7b60e27829b0d875edb162e87b42
Shradha Taneja <shradha.taneja@gmail.com>
2018-12-10 00:56:43 -0500

    filtered and preprocessed text

--- Project/twitter_data/get_tweets.py
+++ Project/twitter_data/get_tweets.py
@@ -1,29 +1,50 @@
 import tweepy
 import pprint
 import config
-
 import pandas as pd
 
-#print(config.CREDENTIALS['access_token'])
-#exit()
+import re
+
 auth = tweepy.OAuthHandler(config.CREDENTIALS['consumer']['key'], config.CREDENTIALS['consumer']['secret'])
 auth.set_access_token(config.CREDENTIALS['access']['token'], config.CREDENTIALS['access']['secret'])
 api = tweepy.API(auth)
 
+
+full_tweets = []
+short_tweet_sentences = []
+long_tweet_sentences = []
+
+#setting character length
+short_sentence_threshold = 55
+
+def preprocess(tweets):
+    for tweet in tweets:
+        tweet_text = tweet._json['full_text']
+
+        filtered_tweet_text = re.sub(r'http\S+', '', tweet_text.replace('\n', ''))
+        full_tweets.append(filtered_tweet_text)
+
+        #assuming sentence ends with '. '
+        tweet_sentences = filtered_tweet_text.split('. ')
+        for sentence in tweet_sentences:
+            if sentence != '':
+                if len(sentence) > short_sentence_threshold:
+                    long_tweet_sentences.append(sentence)
+                else:
+                    short_tweet_sentences.append(sentence)
+
+
+def write_data_to_file(filename, data_set):
+    with open(filename, 'w') as f:
+        for i, data_part in enumerate(data_set):
+            f.write('%i. %s.\n' % (i+1, str(data_part)))
+
+
+
 user = api.get_user('BarackObama')
-#print(user.followers_count)
-data = api.user_timeline(id='BarackObama', count=20, tweet_mode='extended')
+obama_data = api.user_timeline(id='BarackObama', count=20, tweet_mode='extended')
+preprocess(obama_data)
 
-#print(data[0]._json['full_text'])
-
-all_tweets = []
-
-for tweet in data:
-    all_tweets.append(tweet._json['full_text'])
-
-pd_all_tweets = pd.DataFrame(all_tweets)
-print(pd_all_tweets)
-
-pd_all_tweets.to_csv('obama_data.csv')
-
-#print(data)
+write_data_to_file('full_tweets.txt', full_tweets)
+write_data_to_file('short_tweet_sentences.txt', short_tweet_sentences)
+write_data_to_file('long_tweet_sentences.txt', long_tweet_sentences)
